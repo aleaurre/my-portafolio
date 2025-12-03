dc0a69540f020b487e6c2aeb198b4069e624641a
Alexia Aurrecochea <alexia.aurrecochea@correo.ucu.edu.uy>
2025-12-02 23:50:07 -0300

    un proy más

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -100,9 +100,6 @@ export default function Home() {
           </RevealFx>
         </Column>
       </Column>
-      <RevealFx translateY="16" delay={0.6}>
-        <Projects range={[1, 1]} />
-      </RevealFx>
       {routes["/blog"] && (
         <Column fillWidth gap="24" marginBottom="l">
           <Row fillWidth paddingRight="64">
@@ -111,7 +108,7 @@ export default function Home() {
           <Row fillWidth gap="24" marginTop="40" s={{ direction: "column" }}>
             <Row flex={1} paddingLeft="l" paddingTop="24">
               <Heading as="h2" variant="display-strong-xs" wrap="balance">
-                Latest from the blog
+                Lo más reciente del Portafolio
               </Heading>
             </Row>
             <Row flex={3} paddingX="20">
@@ -123,7 +120,6 @@ export default function Home() {
           </Row>
         </Column>
       )}
-      <Projects range={[2]} />
       <Mailchimp />
     </Column>
   );
